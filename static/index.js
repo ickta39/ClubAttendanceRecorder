98b3dbbbd419acf6ecaeead7def3f2683994ba3d
ickta39 <239937122+ickta39@users.noreply.github.com>
2026-05-01 02:14:25 +0900

    ログインしたら名前が出るように

---
+++ static/index.js
@@ -0,0 +1,58 @@
+let profile = null;
+const LOGIN_BUTTON = document.getElementById("login-button")
+const USERNAME_ELEMENT = document.getElementById("profile-name")
+const USERNAME_AREA = document.getElementById("profile-name-area")
+const LOGOUT_BUTTON = document.getElementById("logout-button")
+const HOST_SERVER = getCookie("host-server");
+
+function getCookie(key) {
+    var cookies = document.cookie.split(";")
+    var value = cookies.find(element => element.trim().startsWith(key + "=")).trim()
+
+    return value ? value.substring(key.length + 1, value.length) : null
+}
+
+async function loadProfile(token) {
+    var resp = await fetch(`${HOST_SERVER}/profile`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+    })
+
+    if (!resp.ok) {
+        return null
+    }
+
+    return await resp.json()
+}
+
+addEventListener("load", async (e) => {
+    if (!HOST_SERVER) {
+        location.href = "./login.html"
+        return
+    }
+
+    try {
+        profile = await loadProfile(getCookie("token"))
+        if (profile === null) {
+            location.href = "./login.html"
+            return
+        }
+    }
+    catch {
+
+    }
+
+    window.dispatchEvent(new CustomEvent("profileLoaded", {detail: profile}))
+
+    LOGIN_BUTTON.style.display = "none"
+    USERNAME_ELEMENT.innerText = profile.name
+    USERNAME_AREA.style.display = "unset"
+})
+
+LOGOUT_BUTTON.addEventListener("click", (event) => {
+    document.cookie = "token=; max-age=0"
+    location.href = "./login.html"
+})
